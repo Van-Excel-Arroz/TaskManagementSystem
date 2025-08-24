04cbe528d761170af13d2d4ab3e9ad729c971efd
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-08-24 10:33:32 +0800

    implement generic repository pattern for entities for todoitem, todolist, and user class

---
+++ TaskManagementSystem/Data/GenericRepository.cs
@@ -0,0 +1,65 @@
+﻿using TaskManagementSystem.Models;
+
+namespace TaskManagementSystem.Data
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class, IEntity
+    {
+        private readonly List<T> _items = new();
+        private readonly Dictionary<int, T> _itemDictionary = new();
+        private int _nextId = 1;
+
+        public void Add(T entity)
+        {
+            entity.Id = _nextId++;
+
+            _items.Add(entity);
+            _itemDictionary[entity.Id] = entity;
+
+        }
+
+        public void Remove(int id)
+        {
+            if (DoesIdExist(id))
+            {
+                var item = _itemDictionary[id];
+                _items.Remove(item);
+                _itemDictionary.Remove(id);
+            }
+        }
+
+        public void Update(T entity)
+        {
+
+            if (DoesIdExist(entity.Id))
+            {
+                var itemIndex = _items.FindIndex(item => item.Id == entity.Id);
+
+                if (itemIndex != -1)
+                {
+                    _items[itemIndex] = entity;
+                    _itemDictionary[entity.Id] = entity;
+                }
+            }
+        }
+
+        public T? GetById(int id)
+        {
+
+            if (DoesIdExist(id))
+            {
+                return _itemDictionary[id];
+            }
+            return null;
+        }
+
+        public IReadOnlyList<T> GetAll()
+        {
+            return _items.AsReadOnly();
+        }
+
+        public bool DoesIdExist(int id)
+        {
+            return _itemDictionary.ContainsKey(id);
+        }
+    }
+}
---
+++ TaskManagementSystem/Data/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using TaskManagementSystem.Models;
+
+namespace TaskManagementSystem.Data
+{
+    public interface IGenericRepository<T> where T : class, IEntity
+    {
+        public void Add(T entity);
+
+        public void Remove(int id);
+
+        public void Update(T entity);
+
+        public T? GetById(int id);
+
+        public IReadOnlyList<T> GetAll();
+
+        public bool DoesIdExist(int id);
+
+    }
+}
