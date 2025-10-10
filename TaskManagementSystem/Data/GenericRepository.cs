85b45ed09bb92ee3fe7a2592bc29c92a5b022fe5
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-10-10 16:42:57 +0800

    removed redudant in memory storage List

--- TaskManagementSystem/Data/GenericRepository.cs
+++ TaskManagementSystem/Data/GenericRepository.cs
@@ -4,53 +4,41 @@ namespace TaskManagementSystem.Data
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : class, IEntity
     {
-        private readonly List<T> _items = new();
-        private readonly Dictionary<int, T> _itemDictionary = new();
+        private readonly Dictionary<int, T> _items = new();
         private int _nextId = 1;
 
         public void Add(T entity)
         {
             entity.Id = _nextId++;
-
-            _items.Add(entity);
-            _itemDictionary[entity.Id] = entity;
-
+            _items[entity.Id] = entity;
         }
 
         public void Remove(int id)
         {
-            if (_itemDictionary.TryGetValue(id, out T? item))
+            if (_items.TryGetValue(id, out T? item))
             {
-                _items.Remove(item);
-                _itemDictionary.Remove(id);
+                _items.Remove(id);
             }
         }
 
         public void Update(T entity)
         {
 
-            if (_itemDictionary.TryGetValue(entity.Id, out T? existingItem))
+            if (_items.TryGetValue(entity.Id, out T? existingItem))
             {
-
-                var itemIndex = _items.IndexOf(existingItem);
-
-                if (itemIndex != -1)
-                {
-                    _items[itemIndex] = entity;
-                    _itemDictionary[entity.Id] = entity;
-                }
+                _items[entity.Id] = entity;
             }
         }
 
         public T? GetById(int id)
         {
-            _itemDictionary.TryGetValue(id, out T? item);
+            _items.TryGetValue(id, out T? item);
             return item;
         }
 
         public IReadOnlyList<T> GetAll()
         {
-            return _items.AsReadOnly();
+            return _items.Values.ToList().AsReadOnly();
         }
     }
 }
